9d03a1991237ffb5a2082bbec61adff204c52074
Indeece <anton.shkonda@yandex.ru>
2026-07-05 19:09:11 +0300

    create booking service, configure liquibase

---
+++ hotel-service/src/test/java/ru/indeece/hotelservice/controllers/HotelControllerTest.java
@@ -0,0 +1,26 @@
+package ru.indeece.hotelservice.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void getHotelsByCity() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getHotelById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createHotel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateHotel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteHotel() {
+    }
+}
